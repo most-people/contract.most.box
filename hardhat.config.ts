a6e49dec0b7531b4e13fb8aee54fca5d6c581659
大海 <iwangyang@qq.com>
2024-07-29 05:08:03 +0800

    IPv6

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const PrivateKey = "";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    XLayer: {
+      url: "https://testrpc.xlayer.tech",
+      accounts: [PrivateKey],
+    },
+  },
+};
+
+export default config;
