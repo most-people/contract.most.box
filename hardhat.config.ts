907dc5aff929366a4423bb2eda91e88dc59d23fc
大海 <iwangyang@qq.com>
2025-03-03 18:52:19 +0800

    update PRIVATE_KEY

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
-const PrivateKey = "";
+// 加载 .env 文件
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
+// 确保私钥存在
+if (!PRIVATE_KEY) {
+  console.error("错误: 未设置私钥。请在 .env 文件中设置 PRIVATE_KEY");
+  process.exit(1); // 终止程序
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -11,7 +21,7 @@ const config: HardhatUserConfig = {
     Sepolia: {
       // RPC
       url: "https://sepolia.drpc.org",
-      accounts: [PrivateKey],
+      accounts: [PRIVATE_KEY],
     },
   },
 };
